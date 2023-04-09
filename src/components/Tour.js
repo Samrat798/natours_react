6859f43d8f0f8e987f45e0d727277efcf4776ad9
Samrat798 <majumdersamrat26@gmail.com>
2023-04-09 18:49:45 +0530

    aster

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -10,9 +10,33 @@ const Tour = () => {
         <div className="row">
           <div className="col-1-of-3">
             <div className="card">
-              <div className="card__side card__side--front">Front</div>
+              <div className="card__side card__side--front">
+                <div className="card__picture card__picture--1">&nbsp;</div>
+                <h4 className="card__heading">
+                  <span className="card__heading-span card__heading-span--1">
+                    The sea explorer
+                  </span>
+                </h4>
+                <div className="card__details">
+                  <ul>
+                    <li>3 day tours</li>
+                    <li>up to 30 people</li>
+                    <li>2 tour guides</li>
+                    <li>Sleep in cozy hotels</li>
+                    <li>Defficulty: easy</li>
+                  </ul>
+                </div>
+              </div>
               <div className="card__side card__side--back card__side--back-1">
-                Back
+                <div className="card__cta">
+                  <div className="card__price-box">
+                    <p className="card__price-only">Only</p>
+                    <p className="card__price-value">$297</p>
+                  </div>
+                  <a href="#" className="btn btn--white">
+                    Book now!
+                  </a>
+                </div>
               </div>
             </div>
           </div>
